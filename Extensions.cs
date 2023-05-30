d458b945c83a30ecb5b22198c5f67db486509837
Alexander <satan1c@ukr.net>
2023-05-30 22:55:26 +0300

    fix hsr region resolve

--- Extensions.cs
+++ Extensions.cs
@@ -9,6 +9,9 @@ public static class Extensions
 		return region switch
 		{
 			Region.Europe => "prod_official_eur",
+			Region.America => "prod_official_usa",
+			Region.Asia => "prod_official_asia",
+			Region.Cht => "prod_official_cht",
 			_ => throw new ArgumentOutOfRangeException(nameof(region), region, null)
 		};
 	}
